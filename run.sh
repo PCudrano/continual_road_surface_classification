c67d8f032e6498ac69023f949c16d53ca0a34742
Paolo Cudrano <paolo.cudrano@polimi.it>
2023-07-03 12:16:11 +0000

    Code for ITSC 2023

--- run.sh
+++ run.sh
@@ -4,18 +4,14 @@ run () {
 	strategy=$1
 	i=$2
 	#echo ~/storage/cl_road_pavement/outputs/run_${strategy}_perm${i}.txt
-	run-docker --container_name cudrano_roadpavement_${strategy}_perm${i} 2 64-71 python main_experiments.py -w -v --dsorder 0 1 2 --perm $i $strategy &
+	run-docker --container_name cudrano_roadpavement_${strategy}_perm${i} 7 104-111 python main_experiments.py -w -v --dsorder 0 1 2 --perm $i $strategy &
 	pids[${i}]=$!
 	sleep 5
 	docker logs -f cudrano_roadpavement_${strategy}_perm${i} > ~/storage/cl_road_pavement/outputs/run_${strategy}_perm${i}.log &
 	echo "Started exp perm $i"
 }
 
-#for i in {1..5}; do
-#	run lfl $i
-#done
 run naive 0
-run lfl 0
 
 for pid in $pids; do
 	wait $pid
